6b0e15ebe79c662382a53b82bb306e5221430980
morswin22 <xmorswinx@gmail.com>
2020-07-05 21:20:13 +0200

    :bug: Added missing imports

--- pychroma/__init__.py
+++ pychroma/__init__.py
@@ -1,5 +1,5 @@
 from .Autocomplete import Autocomplete
 from .Connection import Connection, ConnectionError
 from .Controller import Controller
-from .Device import Device
+from .Device import Device, DeviceError, parse_color
 from .Sketch import Sketch
