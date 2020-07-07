55fbd2ce84823dee4aec333d671ddcb9ca533f32
morswin22 <xmorswinx@gmail.com>
2020-07-07 13:30:38 +0200

    :white_check_mark: Added Connection tests

---
+++ tests/test.py
@@ -0,0 +1,27 @@
+import json
+import unittest
+
+from pychroma import Connection, ConnectionError
+
+
+class ConnectionTest(unittest.TestCase):
+  def setUp(self):
+    with open('./tests/example.json', 'r') as file:
+      config = json.load(file)
+
+    self.connection = Connection(config['chroma'])
+
+  def tearDown(self):
+    self.connection.stop()
+
+  def test_wrong_connection_data(self):
+    with self.assertRaises(ConnectionError) as error, open('./tests/example.json', 'r') as file:
+      config = json.load(file)
+      del config['chroma'][list(config['chroma'])[0]]
+      Connection(config['chroma'])
+
+    self.assertEqual(str(error.exception), "Invalid data structure")
+
+  def test_received_url(self):
+    self.assertRegex(self.connection.url, r"http://localhost:[0-9]+/chromasdk")
+    self.assertRegex(self.connection.heartbeat_url, r"http://localhost:[0-9]+/chromasdk/heartbeat")
