d0a81ac192fa17cecef25e7a0cb3651b053126b6
morswin22 <xmorswinx@gmail.com>
2020-07-17 22:49:40 +0200

    :white_check_mark: Updated Connection tests

--- tests/test_0_connection.py
+++ tests/test_0_connection.py
@@ -5,15 +5,6 @@ from pychroma import Connection, ConnectionError
 
 
 class ConnectionTest(unittest.TestCase):
-  def setUp(self):
-    with open('./tests/assets/example.json', 'r') as file:
-      config = json.load(file)
-
-    self.connection = Connection(config['chroma'])
-
-  def tearDown(self):
-    self.connection.stop()
-
   def test_wrong_connection_data(self):
     with self.assertRaises(ConnectionError) as error, open('./tests/assets/example.json', 'r') as file:
       config = json.load(file)
@@ -22,6 +13,29 @@ class ConnectionTest(unittest.TestCase):
 
     self.assertEqual(str(error.exception), "Invalid data structure")
 
-  def test_received_url(self):
-    self.assertRegex(self.connection.url, r"http://localhost:[0-9]+/chromasdk")
-    self.assertRegex(self.connection.heartbeat_url, r"http://localhost:[0-9]+/chromasdk/heartbeat")
+  def test_valid_connection(self):
+    with open('./tests/assets/example.json', 'r') as file:
+      config = json.load(file)
+
+    connection = Connection(config['chroma'])
+    self.assertTrue(connection.alive)
+
+    self.assertFalse(connection.is_connected())
+    self.assertIsNone(connection.url)
+    self.assertIsNone(connection.heartbeat_url)
+    
+    connection.connect()
+
+    self.assertTrue(connection.is_connected())
+    self.assertRegex(connection.url, r"http://localhost:[0-9]+/chromasdk")
+    self.assertRegex(connection.heartbeat_url, r"http://localhost:[0-9]+/chromasdk/heartbeat")
+
+    connection.disconnect()
+
+    self.assertFalse(connection.is_connected())
+    self.assertIsNone(connection.url)
+    self.assertIsNone(connection.heartbeat_url)
+
+    connection.stop()
+
+    self.assertFalse(connection.alive)
