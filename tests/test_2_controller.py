650025401705de242859b5a91713544d2222872c
morswin22 <xmorswinx@gmail.com>
2020-07-21 22:32:09 +0200

    :white_check_mark: Updated Controller tests

--- tests/test_2_controller.py
+++ tests/test_2_controller.py
@@ -8,33 +8,12 @@ class ControllerTest(unittest.TestCase):
     with Controller('./tests/assets/example.json') as controller:
       controller.connect()
 
-      self.assertTrue(isinstance(controller.keyboard, Device))
-      self.assertTrue(isinstance(controller.mouse, Device))
-      self.assertTrue(isinstance(controller.mousepad, Device))
-      self.assertTrue(isinstance(controller.keypad, Device))
-      self.assertTrue(isinstance(controller.headset, Device))
-      self.assertTrue(isinstance(controller.chromalink, Device))
-
-      controller.quit()
-
-  def test_sketch_integration(self):
-    with Controller('./tests/assets/example.json') as controller:
-      controller.run_sketch(Sketch)
-
-      self.assertTrue(isinstance(controller.sketch, Sketch))
-      temp = controller.sketch
-
-      controller.store_sketch()
-
-      self.assertEqual(controller.stored_sketch, temp)
-
-      controller.restore_sketch()
-
-      self.assertEqual(controller.sketch, temp)
-      self.assertEqual(controller.stored_sketch, None)
-
-      controller.idle()
-
-      self.assertFalse(isinstance(controller.sketch, Sketch))
-
-      controller.quit()
+      for device_name in ['keyboard', 'mouse', 'mousepad', 'keypad', 'headset', 'chromalink']:
+        device = None
+        for dev in controller.devices:
+          if dev.name == device_name:
+            device = dev
+            break
+        
+        self.assertIsNotNone(device)
+        self.assertTrue(isinstance(device, Device))
