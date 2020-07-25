96dbafe18df3a7656d4939773b34f73548250334
morswin22 <xmorswinx@gmail.com>
2020-07-26 00:03:33 +0200

    :sparkles: Add setup.py for PyPI

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pychroma",
+    version="0.0.1",
+    author="Patryk Janiak",
+    author_email="xmorswinx@gmail.com",
+    description="Python library for accessing the Razer Chroma SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/morswin22/pychroma",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
